ce1e5375634a2f3669a513a083f949eca15c9f17
yabenman <benmansour.yassinne@gmail.com>
2025-01-04 02:32:25 +0100

    update

--- Level2/ft_atoi.c
+++ Level2/ft_atoi.c
@@ -1,22 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yabenman <yabenman@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/03 22:43:52 by yabenman          #+#    #+#             */
-/*   Updated: 2025/01/03 23:02:56 by yabenman         ###   ########.fr       */
+/*   Created: 2025/01/03 22:49:06 by yabenman          #+#    #+#             */
+/*   Updated: 2025/01/03 23:02:53 by yabenman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int	ft_strcmp(char *s1, char *s2)
+int	ft_atoi(char *str)
 {
-	int i;
+	int	i;
+	int	sign;
+	int	result;
+
 	i = 0;
-	while (s1[i] && s2[i] && (s1[i] == s2[i]))
+	sign = 1;
+	result = 0;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
 		i++;
-	return (s1[i] - s2[i]);
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+		{
+			sign = -1;
+			i++;
+		}
+		else if (str[i] == '+')
+			i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (result * sign);
 }
---
+++ Level2/ft_strdup.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yabenman <yabenman@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/04 00:35:55 by yabenman          #+#    #+#             */
+/*   Updated: 2025/01/04 00:38:32 by yabenman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	char *dest;
+	int i;
+	int len;
+
+	while (src[len])
+		len++;
+	dest = (char *)malloc(sizeof(char) * len + 1);
+	i = 0;
+	if (dest == NULL)
+		return (NULL);
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
